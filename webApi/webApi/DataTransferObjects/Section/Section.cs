940206bdff194a1361bb7fb2e6ff18eb629cadf0
Mikołaj Ryll <01142207@pw.edu.pl>
2021-03-23 21:49:51 +0100

    section dto implemented

--- webApi/webApi/DataTransferObjects/Section/Section.cs
+++ webApi/webApi/DataTransferObjects/Section/Section.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using webApi.DataTransferObjects.Dish;
 
 namespace webApi.DataTransferObjects.Section
 {
     public class Section
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int RestaurantId { get; set; }
+        [Required]
+        public ICollection<PositionFromMenu> Positions { get; set; }
     }
 }
