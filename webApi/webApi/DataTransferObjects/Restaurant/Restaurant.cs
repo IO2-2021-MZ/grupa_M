5a43b2e89e8e7c772eb4e3d14f8b15b212bb586b
Mikołaj Ryll <01142207@pw.edu.pl>
2021-03-23 20:58:04 +0100

    quick database fix

---
+++ webApi/webApi/DataTransferObjects/Restaurant/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApi.DataTransferObjects.Restaurant
+{
+    public class Restaurant
+    {
+
+    }
+}
