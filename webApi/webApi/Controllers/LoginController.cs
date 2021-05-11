62b489cfb6f122a36cbb8b1eb38e1e6d77ec4c15
b.lusztak <b.lusztak@student.mini.pw.edu.pl>
2021-05-11 03:43:43 +0200

    Added Structured Components for Authentication Process

---
+++ webApi/webApi/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using webApi.DataTransferObjects.AuthenticateDTO;
+using webApi.Helpers;
+using webApi.Services;
+
+namespace BackEnd.Controllers
+{
+    
+    [ApiController]
+    public class LoginController : BaseController
+    {
+
+        private readonly IAccountService _accountService;
+        
+
+
+        public LoginController(IAccountService service)
+        {
+            _accountService = service;
+        }
+
+        [HttpPost("Test")]
+        public IActionResult Test()
+        {
+            return Ok("Ok");
+        }
+
+        /// <summary>
+        /// Zwraca dane o użytkowniku wraz z krótkoterminowym tokenem dostępu i 
+        /// długoterminowym refresh tokenem umiesczonym w plikach cookies. 
+        /// </summary>
+        /// <param name="value"> Email i hasło użytkownika </param>
+        /// <returns> Dane o użytkowniku </returns>
+        [HttpPost("SignIn")]
+        public IActionResult SignIn([FromBody] AuthenticateRequest value)
+        {
+            var response = _accountService.Authenticate(value, ipAddress());
+            return Ok(response);
+        }
+
+        private string ipAddress()
+        {
+            if (Request.Headers.ContainsKey("X-Forwarded-For"))
+                return Request.Headers["X-Forwarded-For"];
+            else
+                return HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+        }
+    }
+}
+
---
+++ webApi/webApi/DataTransferObjects/AuthenticateDTO/AuthenticateResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace webApi.DataTransferObjects.AuthenticateDTO
+{
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Token { get; set; }
+    }
+}
