bf2e2d7ff4a10fa80bc1c8b55341945ce8a6566e
xsnailu <xsnailu@gmail.com>
2021-06-09 23:02:48 +0200

    user/employee restaurant has address

--- webApi/webApi/Controllers/UserController.cs
+++ webApi/webApi/Controllers/UserController.cs
@@ -57,7 +57,14 @@ namespace webApi.Controllers
             response.isRestaurateur = user.Role == (int)Role.restaurateur ? true : false;
             var usr = _context.UserRests.Include(u => u.Restaurant).Where(u => u.UserId == user.Id).FirstOrDefault();
             var mapped = _mapper.Map<RestaurantDTO>(usr == null ? user.Restaurant : usr.Restaurant);
-            // tutaj trzeba dodac adres
+            int restaurantAddresId = usr == null ? user.Restaurant.AddressId : usr.Restaurant.AddressId;
+            var address = _context
+                .Addresses
+                .Where(a => a.Id == restaurantAddresId)
+                .FirstOrDefault();
+            var addresDto = _mapper.Map<AddressDTO>(address);
+            mapped.Address = addresDto;
+
             response.restaurant = mapped;
             return Ok(response);
         }
